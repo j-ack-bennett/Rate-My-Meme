e15285bd0110655e22c12e7ba7bba4f320f141fd
Lara Hayward <haywardlbl@gmail.com>
2021-01-21 23:07:53 +1300

    adding edgy memes

--- seeds/1-memes.js
+++ seeds/1-memes.js
@@ -6,5 +6,11 @@ exports.seed = function (knex, Promise) {
     {id: 4, url: "/pcbuild.jpg", submitted_by: "jack", category_id: 3},
     {id: 5, url: "/twosmirnofs.jpg", submitted_by: "jack", category_id: 3},
     {id: 6, url: "/whyareyourunning.jpg", submitted_by: "jack", category_id: 3},
+    {id: 7, url: "/deadinside.jpg", submitted_by: "lara", category_id: 2},
+    {id: 8, url: "/netflix.jpg", submitted_by: "lara", category_id: 2},
+    {id: 9, url: "/neverknownhappiness.jpg", submitted_by: "lara", category_id: 2},
+    {id: 10, url: "/po.jpg", submitted_by: "lara", category_id: 2},
+    {id: 11, url: "/rosesarered.jpg", submitted_by: "lara", category_id: 2},
+    {id: 12, url: "/weightedblanket.jpg", submitted_by: "lara", category_id: 2},
   ])
 }
