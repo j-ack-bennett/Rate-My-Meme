b77df4ce9e7d45f5497194457e0a56985203bfa2
student <student@eda-bc-06.localdomain>
2021-01-22 13:24:51 +1300

    heroku deployment changes

--- knexfile.js
+++ knexfile.js
@@ -24,17 +24,16 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
       tableName: 'knex_migrations'
+      // tableName: '20160802144759_memes',
+      // tableName: '20210121205758_categories',
+      // tableName: '20210121210049_comments'
     }
   }
 }
